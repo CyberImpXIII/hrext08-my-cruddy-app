b6e9dfca336743935c2367876c8d636816c5677e
Jacob <Specter@Jacobs-MacBook-Pro-2.local>
2019-04-03 16:29:27 -0600

    added functionality in preparation to make it more like a blog editor.  Current table only displays last updated time but localStorage still stores

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ var loadLocalStorage = function () {
 	var keys = Object.keys(localStorage)
 	var htmlString = '';
 	for (var i = 0; i < keys.length; i++) {
-		htmlString += `<tr><td>${keys[i]}</td><td>${localStorage[keys[i]]}</tr></tr>`;
+		htmlString += `<tr ><td class="entry" id="${keys[i]}">${keys[i]}</td><td>` + JSON.parse(localStorage.getItem(keys[i]))[1] + '</tr></tr>';
 	}
 	$('tbody').html(htmlString)
 };
@@ -16,78 +16,81 @@ var updateStatusLabel = function(message) {
  // logic for determining action probably needs to go in the event handler
 $(document).ready(function readyFunc() {
 	loadLocalStorage();
+	var homepage = $('body');
 
-	$('#key').on('input',function(e){
-    var key = $('#key').val();
-    console.log($('#key').val());
-    var keyExists = (localStorage.getItem(key)!==null);
-    if(keyExists){
-    	$("#all-buttons").html(`<button class='button' id='btn-update' type='button'>Update</button> 
-    		<button class='button' id='btn-delete' type='button'>Delete</button>`);
-    	readyFunc();
-    }else{
-    	if($("#btn-update")){
-    		$("#all-buttons").html(`<button class="button" id="btn-create" type="button">Create</button> 
-    		<button class='button' id='btn-delete' type='button'>Delete</button>`);
-    		readyFunc();
-    	}
-    }
-	});
+	function buttonFunctionality(){
+		$('#key').on('input',function(e){
+    		var key = $('#key').val();
+    		var keyExists = (localStorage.getItem(key)!==null);
+    		if(keyExists){
+    			$("#all-buttons").html(`<button class='button' id='btn-update' type='button'>Update</button> 
+    			<button class='button' id='btn-delete' type='button'>Delete</button>`);
+    			buttonFunctionality();
+    		}else{
+    			if($("#btn-update")){
+    				$("#all-buttons").html(`<button class="button" id="btn-create" type="button">Create</button> 
+    				<button class='button' id='btn-delete' type='button'>Delete</button>`);
+    				buttonFunctionality();
+    			}
+    		}
+		});
 
-	$('#btn-create').on('click', function(e) {
-		var key = $('#key').val();
-		var value = $('#value').val();
-		var keyExists = localStorage.getItem(key) !== null;
+		$('#btn-create').on('click', function(e) {
+			var key = $('#key').val();
+			var value = $('#value').val();
+			var keyExists = localStorage.getItem(key) !== null;
 
-		if (keyExists) {
-			updateStatusLabel('key already exists, please use update button instead! :D');
-		} else if (key === '') {
-			updateStatusLabel('invalid input!')
-		}else {
-			createEntry(key, value);
-			updateStatusLabel('key created - ' + key);
-		}
+			if (keyExists) {
+				updateStatusLabel('key already exists, please use update button instead! :D');
+			} else if (key === '') {
+				updateStatusLabel('invalid input!')
+			}else {
+				createEntry(key, JSON.stringify([value, Date()]));
+				updateStatusLabel('key created - ' + key);
+			}
 
-		loadLocalStorage();
-	});
+			loadLocalStorage();
+		});
 
-	$('#btn-update').on('click', function(e) {
-		var key = $('#key').val();
-		var value = $('#value').val();
-		var existingValue = localStorage.getItem(key)
-		var keyExists = existingValue !== null;
+		$('#btn-update').on('click', function(e) {
+			var key = $('#key').val();
+			console.log(key);
+			var value = $('#value').val();
+			var existingValue = localStorage.getItem(key)
+			var keyExists = existingValue !== null;
 
-		if (value === existingValue) {
-			updateStatusLabel('key not updated - that value already exists silly! xD')
-		} else if (keyExists) {
-			updateEntry(key, value);
-			updateStatusLabel('key updated - ' + key);
-		} else if (key === '') {
-			updateStatusLabel('invalid input!')
-		} else {
-			updateStatusLabel('key doesn\'t exist, please use create button instead! :D');
-		}		
-		
-		loadLocalStorage();		
-	});
+			if (value === existingValue) {
+				updateStatusLabel('key not updated - that value already exists silly! xD')
+			} else if (keyExists) {
+				updateEntry(key, JSON.stringify([value, Date(), JSON.parse(localStorage.getItem(key))[1]]));
+				updateStatusLabel('key updated - ' + key);
+			} else if (key === '') {
+				updateStatusLabel('invalid input!')
+			} else {
+				updateStatusLabel('key doesn\'t exist, please use create button instead! :D');
+			}		
+			
+			loadLocalStorage();		
+		});
 
-	$('#btn-delete').on('click', function(e) {
-		var key = $('#key').val();
-		var value = $('#value').val();
-		var keyExists = localStorage.getItem(key) !== null;
+		$('#btn-delete').on('click', function(e) {
+			var key = $('#key').val();
+			var value = $('#value').val();
+			var keyExists = localStorage.getItem(key) !== null;
 
-		if (keyExists) {
-			removeEntry(key);
-			updateStatusLabel('key removed - ' + key);
-		} else if (key === '') {
-			updateStatusLabel('invalid input!')
-		} else {
-			updateStatusLabel('key doesn\'t exist, nothing removed. :|');
-		}
+			if (keyExists) {
+				removeEntry(key);
+				updateStatusLabel('key removed - ' + key);
+			} else if (key === '') {
+				updateStatusLabel('invalid input!')
+			} else {
+				updateStatusLabel('key doesn\'t exist, nothing removed. :|');
+			}
 
-		loadLocalStorage();
-	});	
-
+			loadLocalStorage();
+		});	
+	};
+	buttonFunctionality();
 });
 /*
 
